5ea26c1c5712264c11701e7c7ea3dfd223d7c056
yusufgozukara <yusufgozukara11@gmail.com>
2022-09-07 22:42:25 +0300

    blog models started

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Blog(models.Model):
+
+
+
+    user=models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
