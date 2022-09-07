dd1d87d65f3a2b379fcec7ba99a2855401b539f5
yusufgozukara <yusufgozukara11@gmail.com>
2022-09-07 17:42:51 +0300

    users urls and register func added

--- main/urls.py
+++ main/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls'))
 
 ]
