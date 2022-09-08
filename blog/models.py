5915474e0f9a2535cf07208fcc3a3dd9f0906a90
yusufgozukara <yusufgozukara11@gmail.com>
2022-09-08 08:47:04 +0300

    blog model added

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,31 @@
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 
 
 # Create your models here.
 
+blog_category = [
+    ("category_1", "İnsan"),
+    ("category_2", "Doğa"),
+    ("category_3", "Yaşam"),
+    ("category_4", "Sağlık"),
+    ("category_5", "Teknoloji"),
+]
+
+blog_status = [
+    ("status_1", "Draft"),
+    ("status_2", "Published"),
+]
+
 class Blog(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    blog_pic = models.ImageField()
+    category = models.CharField(max_length=20, choices=blog_category)
+    status = models.CharField(max_length=20, choices=blog_status)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
 
 
