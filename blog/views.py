a0a1f39bf23e593ed878512cf8696584a7daf2d6
yusufgozukara <yusufgozukara11@gmail.com>
2022-09-09 08:28:03 +0300

    blog view_first commit

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blog
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hoşgeldin Başgan')
+    blogs = Blog.objects.all()
+    context = {
+        'blogs' : blogs
+    }
+    return render(request, 'blog/home.html', context)
